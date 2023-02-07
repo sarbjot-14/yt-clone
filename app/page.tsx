af9fd9c9916cdd6b4926cc7bd81662cf6163cb95
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-02-06 16:58:14 -0800

    side bar styling done

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import styles from './page.module.css';
 import Login from '@/common/components/authentication/Login';
 import { dbFindUser } from '@/common/db-queries';
 import VideoUpload from '@/common/components/feature/videoUpload';
+import SideMenu from '@/common/components/layout.tsx/sideMenu';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,13 +19,25 @@ export default async function Home() {
   const dataDb = await getData();
   console.log('dataDb,', dataDb);
   const data = { id: 1 };
+  let isOpen: boolean = true;
+  const toggleSideMenu = () => {
+    isOpen = !isOpen;
+  };
 
   return (
     <main className={styles.main}>
-      <h1>dataDb {dataDb.name}</h1>
-      <Login></Login>
-      <VideoUpload></VideoUpload>
-      <h1 className="bg-blue-700">hahahah okay </h1>
+      <div className="w-full flex ">
+        <div className="flex-none w-100 h-screen">
+          <SideMenu isOpen={isOpen}></SideMenu>
+        </div>
+
+        <div className="bg-green-500 flex-1 ">
+          <h1>dataDb {dataDb.name}</h1>
+          <Login></Login>
+          <VideoUpload></VideoUpload>
+          <h1 className="bg-blue-700">hahahah okay </h1>
+        </div>
+      </div>
     </main>
   );
 }
---
+++ common/components/layout.tsx/sideMenu.tsx
@@ -0,0 +1,135 @@
+import Image from 'next/image';
+import React from 'react';
+import Home from '../application/icons/home';
+
+const SideMenu = ({ isOpen }: { isOpen: boolean }) => {
+  return isOpen ? (
+    // <div>overflwo hidden not working</div>
+    <div className="h-screen w-full ">
+      <div className="  ">
+        <button className="flex justify-start items-center gap-5 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Home></Home>
+
+          <h3 className="inline w-500 px-100">Home</h3>
+        </button>
+        <button className="flex justify-start items-center gap-1 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/shorts.svg"
+            alt="/icons/shorts.svg"
+          ></Image>
+
+          <h3 className="inline w-20 px-100">Shorts</h3>
+        </button>
+        <button className="flex justify-start items-center gap-4 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/subscriptions.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-20 px-100">Subscriptions</h3>
+        </button>
+        <button className="flex justify-start items-center gap-2 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/orginals.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-20 px-100">Originals</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/youtubeMusic.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Youtube Music</h3>
+        </button>
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/library.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Library</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/history.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">History</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/yourVideos.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Your Videos</h3>
+        </button>
+        <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <h3 className="px-8">Explore</h3>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/trending.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Trending</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/music.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Music</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/Live.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">Live</h3>
+        </button>
+        <button className="flex justify-start items-center gap-3 p-5 m-3 pr-20 h-5 rounded-lg hover:bg-gray-100 ">
+          <Image
+            width="27"
+            height="30"
+            src="/icons/News.svg"
+            alt="/icons/youtube.svg"
+          ></Image>
+
+          <h3 className="inline w-25 px-100">News</h3>
+        </button>
+      </div>
+    </div>
+  ) : (
+    <div>Closed</div>
+  );
+};
+
+export default SideMenu;
