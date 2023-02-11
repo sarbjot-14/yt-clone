681cd59411cb2a84aec249b97f97ff625f0652f1
sarbjot-konectAI <sarbjot.s@konect.ai>
2023-02-10 20:13:04 -0800

    forced string

--- common/components/authentication/ProfileMenu.tsx
+++ common/components/authentication/ProfileMenu.tsx
@@ -101,7 +101,7 @@ const ProfileMenu = () => {
             className="h-10 w-10 relative"
           >
             <Image
-              src={session.user.image}
+              src={session.user.image!}
               alt="/icons/shorts.svg"
               layout="fill" // required
               objectFit="cover" // change to suit your needs
